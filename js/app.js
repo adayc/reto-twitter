3a9aca70ec21f52158cb98deec4a87eae9612510
adayc <adayc@hotmail.com>
2017-11-17 00:55:14 -0500

    Version 0.0.1 - sin CSS

--- js/app.js
+++ js/app.js
@@ -6,17 +6,15 @@ var alltweets = document.getElementById('tweet-content');
 
 window.addEventListener('load', function(event) {
  
-  function addTweet(text) {
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML = text;
-    newDiv.classList.add('tweet');
-    alltweets.insertBefore(newDiv, alltweets.childNodes[0]);
-    tweet.innerHTML ='';
-  }
-
 
 
   btnTwittear.addEventListener('click', function(event) {
-    addTweet(tweet.innerHTML);
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = tweet.innerHTML;
+    newDiv.classList.add('tweet');
+    alltweets.insertBefore(newDiv, alltweets.childNodes[0]);
+    tweet.innerHTML ='';
   });
+
+  
 });
